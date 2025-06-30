8f37832754d163494639019d4cbe26ecc1aa9378
Pramod <pramodkoppad98@gmail.com>
2025-06-30 19:38:09 +0530

    added hours

--- timer.py
+++ timer.py
@@ -3,7 +3,8 @@ my_time= int(input("Enter the time in seconds: "))
 for x in range(my_time,0,-1):
     seconds =x %60
     minutes =int(x/60)%60
-    hours=int (x/3600)
-    print(f"{hours:02}:{minutes:02}:{seconds:02}",end="\r") 
+    hours=int (x/3600)%24
+    days=int(x/86400)
+    print(f"{days:02}:{hours:02}:{minutes:02}:{seconds:02}",end="\r") 
     time.sleep(1)
 print("The time is finished")
